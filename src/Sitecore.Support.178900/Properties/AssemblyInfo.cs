33b2238a2d653d4adf7fb2713e1a058545a33848
DK\yho <yho@sitecore.net>
2017-08-17 11:09:23 +0800

    Commit patch master branch

---
+++ src/Sitecore.Support.178900/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.178900")]
+[assembly: AssemblyProduct("Sitecore.Support.178900")]
+[assembly: ComVisible(false)]
